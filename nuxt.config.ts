97d2c3e4547bb845a9a2b9e1adc201530611089c
Marty <MLoth@users.noreply.github.com>
2024-09-02 10:02:24 +0200

    chore: added bunch of config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  experimental: {
+    componentIslands: true,
+    viewTransition: true,
+  },
+
+  typescript: {
+    strict: true,
+  },
+
+  modules: ['@nuxt/eslint'],
+
+  eslint: {
+    config: {
+      typescript: true,
+    },
+
+    checker: {
+      fix: true,
+    },
+  },
 })
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Jow</h1>
+</template>
