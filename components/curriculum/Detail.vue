0019c7a785dda3a00e6b3947386d81eeb9c796ff
Marty <MLoth@users.noreply.github.com>
2024-12-04 11:00:45 +0100

    feat: added correct colors to modules

--- components/curriculum/Detail.vue
+++ components/curriculum/Detail.vue
@@ -1,13 +1,18 @@
 <template>
   <RouterLink
-    :class="`rounded-2xl bg-neutral-50 px-4 py-4 hover:text-neutral-400 row-span-${
+    :class="`rounded-2xl px-4 py-4 row-span-${
       props.module.studycredits / 3
     } col-start-${props.module.semester} ${
       props.module.tracks &&
       props.module.tracks.map((track: string) => ` js-track-${track}`).join(``)
-    } bg-${props.module.pillar}-400`"
+    } bg-${props.module.pillar}-100`"
     :to="`${props.module._path}`"
   >
+    <!-- <span
+      :class="`bg-${props.module.pillar}-500 rounded-2xl px-3 py-1 text-sm text-white`"
+    >
+      {{ props.module.pillar }}
+    </span> -->
     <h1 class="font-bold">{{ props.module.title }}</h1>
   </RouterLink>
 </template>
